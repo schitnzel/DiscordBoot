d49251c7e427f57cd3ff596aad1bafb10713fce1
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-05-17 17:40:53 -0300

    Update interactionCreate.js

--- src/events/client/interactionCreate.js
+++ src/events/client/interactionCreate.js
@@ -1,5 +1,6 @@
 const Event = require('../../structures/Event')
 const User = require('../../structures/models/Users')
+const Guild = require('../../structures/models/Guilds')
 
 module.exports = class extends Event {
     constructor(client) {
@@ -9,7 +10,36 @@ module.exports = class extends Event {
     }
     run = async (interaction) => {
         if (interaction.isCommand()) {
+          const guild = await Guild.findOne({ _id: interaction.guild.id });
           const user = await User.findOne({_id: interaction.member.id});
+          if (!guild) {
+		new Guild({
+			_id: interaction.guild.id,
+			name: interaction.guild.name,
+			icon: interaction.guild.icon ? interaction.guild.iconURL({ size: 4096 }) : "https://cdn.discordapp.com/embed/avatars/0.png",
+			bio: "Um servidor :D",
+			verified: false,
+			config: {
+				welcome: {
+					channel: "",
+					message: "{\"content\":\"Bem vindo(a) {user.username} ao servidor!\"}"
+				},
+				bye: {
+					channel: "",
+					message: "{\"content\":\"{user.username} saiu do servidor.\"}"
+				},
+				system: {
+					antispam: {
+						check: false,
+						config: {
+							blacklist: []
+						}
+					},
+					antilink: false
+				}
+			}
+		}).save()
+	}
           if(!user) {
            new User({_id: interaction.member.id,
 			lang: "pt-br",
