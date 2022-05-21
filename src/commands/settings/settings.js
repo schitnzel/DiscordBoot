d0f57bad85784271028ee8e5e69833ecea8bea80
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-05-21 19:56:22 -0300

    Update settings.js

--- src/commands/settings/settings.js
+++ src/commands/settings/settings.js
@@ -13,7 +13,7 @@ module.exports = class extends Command {
      run = async (interaction) => {
         const user = await Users.findOne({_id: interaction.member.id});
         const lang = this.client.lang({lang: user.lang, cmd: 'settings'})
-        const json = JSON.parse(lang.text)
+        const json = JSON.parse(JSON.stringify(lang.text))
         interaction.reply(json)
      }
 }
