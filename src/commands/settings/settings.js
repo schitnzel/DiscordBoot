49396773cb6b114f4f480ed6ed7a5375b7a68cce
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-05-21 22:50:10 +0000

    .

--- src/commands/settings/settings.js
+++ src/commands/settings/settings.js
@@ -1,16 +1,18 @@
 const Command = require('../../structures/Command')
 const Users = require('../../structures/models/Users')
+const { MessageActionRow, MessageButton } = require('discord.js');
+
 
 module.exports = class extends Command {
     constructor(client) {
         super(client, {
-            name: 'language',
-            description: 'Altere idioma do bot para seu cliente.',
+            name: 'settings',
+            description: 'Configure seu servidor!',
         })
       }
      run = async (interaction) => {
         const user = await Users.findOne({_id: interaction.member.id});
         const lang = this.client.lang({lang: user.lang, cmd: 'settings'})
-        interaction.reply(lang.text)
+        
      }
 }
