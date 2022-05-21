23eb9c8dc07a6e237851c359d16690524b597859
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-05-21 19:49:09 -0300

    Create settings.js

---
+++ src/structures/Lang/pt-br/settings.js
@@ -0,0 +1,11 @@
+{
+ "text": {
+          "content": "Configurações do servidor", 
+          "embeds":[
+                 {"title":"Configurando {server.name}", 
+                  "description": "Configure seu servidor através do bot, ao envés de acessar um website, tenha uma configuração mais fácil e rápido!",
+                  "thumbnail": "{server.icon}",
+                  "color": "#0099ff"}
+                 ]
+         }
+}
