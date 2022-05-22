415f1ace8b6a4801ebff7041d5059e2a8d828dde
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-05-22 11:13:47 -0300

    Update animal.js

--- src/commands/fun/animal.js
+++ src/commands/fun/animal.js
@@ -1,1 +1,25 @@
-// Bur bur stinks
+const animality = require('animality');
+
+const Command = require('../../structures/Command')
+const Users = require('../../structures/models/Users')
+
+module.exports = class extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'animal',
+            description: 'Fotinhas de animais',
+            options: [{
+              name: 'animal',
+              type: 3,
+              required: false,
+              choices: [{name: "Gato", value: 'cat'}]
+            }]
+        })
+    }
+    run = async (interaction) => {
+        const user = await Users.findOne({_id: interaction.member.id});
+        const lang = this.client.lang({lang: user.lang, cmd: 'animal'});
+        const animal = async animality.getAsync(interaction.options.getString('animal'), process.env.apikey);
+        interaction.reply({embeds: [{title: lang.text[animal.name] || "🐇 | Animal", image: {url: animal.url}}]})
+    }
+}
